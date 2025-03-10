68799ceb279751282eecd45510522dcea8238f34
Devanshu <devanshu2343@gmail.com>
2025-03-11 00:01:31 +0530

    about section added

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    // return view ('folderName.(dot)fileName') ;
+    return view('about.about' , ["name" => "Dev"]);
+});
